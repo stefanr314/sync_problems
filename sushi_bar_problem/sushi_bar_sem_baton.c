7c0a643da2c14089551bde4eaf80c7813ea383ae
stefanr <stefanristic01@yahoo.com>
2026-05-14 17:39:37 +0200

    solve sushi problem.

--- sushi_bar_problem/sushi_bar_sem_baton.c
+++ sushi_bar_problem/sushi_bar_sem_baton.c
@@ -18,7 +18,11 @@ void clean();
 void lunch();
 
 sem_t emptied;
+sem_t accu_barrier;
 sem_t mutex;
+sem_t extreme_case_barrier;
+pthread_mutex_t visualMutex;
+int gangOfFive;
 int dr;
 int seatsAvailable;
 
@@ -32,6 +36,8 @@ int main() {
     Pthread_create(&threads[i], customer, id);
   }
 
+  sem_post(&accu_barrier);
+
   for (int i = 0; i < TOTAL_GUESTS; i++) {
     int result = pthread_join(threads[i], NULL);
     assert(result == 0);
@@ -43,9 +49,12 @@ int main() {
 }
 
 void *enter_the_restaurant(void *arg) {
+  int id = *(int *)arg;
   sem_wait(&mutex);
   if (seatsAvailable == 0 || dr > 0) {
     dr++;
+    printf("Customer with id: %d is waiting infront of restaurant: %d\n", id,
+           dr);
     sem_post(&mutex);
     sem_wait(&emptied);
     dr--;
@@ -70,21 +79,34 @@ void *leave_the_restaurant(void *arg) {
   return NULL;
 }
 
+/* barrier is not reusable here but thats fine it will run just once and get
+ * destroyed*/
 void *customer(void *arg) {
-  int id = *(int*)arg;
+  int id = *(int *)arg;
 
-  printf("Customer with id: %d just entered the restaurant\n", id);
-  enter_the_restaurant(NULL);
+  printf("Hit the accu barrier\n");
+
+  sem_wait(&accu_barrier);
+  sem_post(&accu_barrier);
+
+  enter_the_restaurant(arg);
+  lunch();
   leave_the_restaurant(NULL);
+
   printf("Customer with id: %d just left the restaurant\n", id);
 
+  free(arg);
   return NULL;
 }
 void init_state() {
   dr = 0;
   seatsAvailable = SEATS_AVAILABLE;
+  gangOfFive = 0;
 
+  pthread_mutex_init(&visualMutex, NULL);
+  sem_init(&extreme_case_barrier, 0, 0);
   sem_init(&mutex, 0, 1);
+  sem_init(&accu_barrier, 0, 0);
   sem_init(&emptied, 0, 0);
 }
 
@@ -108,5 +130,6 @@ void Pthread_create(pthread_t *tid, void *(*routine)(void *), void *arg) {
 
 void clean() {
   sem_destroy(&mutex);
+  sem_destroy(&accu_barrier);
   sem_destroy(&emptied);
 }
